a8ad21d40969a76a1d87be73071f22311b05df21
Matt B <110642574+BeitM@users.noreply.github.com>
2024-12-15 09:38:28 -0500

    park automode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompOpModes/Autonomous/OpModes/AutoObservationZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompOpModes/Autonomous/OpModes/AutoObservationZone.java
@@ -1,31 +1,178 @@
 package org.firstinspires.ftc.teamcode.CompOpModes.Autonomous.OpModes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.CompOpModes.Autonomous.Positions.AutoPositionsObservation;
+import org.firstinspires.ftc.teamcode.Shoddy.ShoddyAnalog;
+import org.firstinspires.ftc.teamcode.Shoddy.ShoddyPositions;
+import org.firstinspires.ftc.teamcode.Shoddy.ShoddyRobotClass;
+import org.firstinspires.ftc.teamcode.Shoddy.ShoddyRobotClassAuto;
+import org.firstinspires.ftc.teamcode.Shoddy.ShoddyTeleOp;
+import org.firstinspires.ftc.teamcode.Utility.AbsoluteAnalogEncoder;
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.PoseUpdater;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.DashboardPoseTracker;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Drawing;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
 
 import org.firstinspires.ftc.teamcode.CompOpModes.Autonomous.Positions.AutoPositionsNet;
-import org.firstinspires.ftc.teamcode.CompOpModes.Autonomous.Positions.AutoPositionsObservation;
-import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
-import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
 
 @Config
 @Autonomous(name="AutoObservationZone", group="Auto")
 
 public class AutoObservationZone extends OpMode
 {
-    AutoPositionsObservation o = new AutoPositionsObservation();
+    public ShoddyRobotClassAuto r;
+    public ShoddyPositions po;
+    public AutoPositionsObservation o;
+
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
-    private int pathState;
+    public boolean holdPos = true;
 
-    //Subsystems here
+    public static VoltageSensor voltageSensor;
+
+    public int xOffset;
+    public int yOffset;
+    public int hOffset;
+
+    private PoseUpdater poseUpdater;
+    private DashboardPoseTracker dashboardPoseTracker;
+
+    //Other Shit
+
+    double leftLinearTarget;
+    double rightLinearTarget;
+    double clawTarget;
+    double wristTarget;
+
+    double botVerticalPower = 0;
+    double topVerticalPower = 0;
+    double intakePower = 0;
+
+    public boolean usePIDFvertical = true;
+    public boolean usePIDFswivel = true;
+    public boolean usePIDFV4B = true;
+
+    //First PID for V4B
+    private PIDController controller;
+    public static double p = 0.005, i = 0.01, d = 0.00004;
+    public static double f = 0.06;
+    private final double ticks_in_degree = 144.0 / 180.0;
+    public int V4BTarget;
+    double armPos;
+    double pid, targetArmAngle, ff, currentArmAngle, V4BPower;
+
+    //Second PID for Vertical Slides
+    private PIDController controller2;
+    public static double p2 = 0.006, i2 = 0.001, d2 = 0;
+    public static double f2 = 0;
+    private final double ticks_in_degree2 = 144.0 / 180.0;
+    public int vertSlidesTarget;
+    double armPos2;
+    double pid2, targetArmAngle2, ff2, currentArmAngle2, verticalSlidesPower;
+
+    //Third PID for Swivel
+    private PIDController controller3;
+    public static double p3 = -0.006, i3 = 0.02, d3 = 0.0002;
+    public static double f3 = 0.035;
+    private final double ticks_in_degree3 = 144.0 / 180.0;
+    public int swivelTarget;
+    double armPos3;
+    double pid3, targetArmAngle3, ff3, currentArmAngle3, swivelPower;
+
+    public int TimeVar = 100;
+    public boolean loopDone = false;
+    int extendTime = 400;
+    int transferTime = 2000;
+    int dropTime = 1000;
+    int grabTime = 1800;
+
+    int currentSample = 0;
+
+    public PathState pathState;
+    public enum PathState {
+
+        GET1,
+        GET2,
+        GET3,
+
+        //Other
+        PARK,
+        END,
+        DELAY
+    }
+    public enum IntakeState {
+        INTAKE_START,
+        INTAKE_EXTEND,
+        INTAKE_GRAB,
+        INTAKE_RETRACT,
+        INTAKE_IDLE2, INTAKE_IDLE
+    };
+
+    public enum OuttakeState {
+        OUTTAKE_START,
+        OUTTAKE_EXTEND,
+        OUTTAKE_SWIVEL,
+        OUTTAKE_IDLE
+    };
+
+    public enum TransferState {
+        TRANSFER_START,
+        TRANSFER_INTAKE,
+        TRANSFER_CLAW,
+        TRANSFER_OUTTAKE,
+        TRANSFER_IDLE
+    };
+
+    public enum OuttakeState2 {
+        OUTTAKE2_START,
+        OUTTAKE2_RETRACT,
+        OUTTAKE2_IDLE
+    }
+    public IntakeState intakeState = IntakeState.INTAKE_IDLE;
+    public ElapsedTime intakeTimer = new ElapsedTime();
+    public OuttakeState outtakeState = OuttakeState.OUTTAKE_IDLE;
+    public ElapsedTime outtakeTimer = new ElapsedTime();
+    public TransferState transferState = TransferState.TRANSFER_IDLE;
+    public ElapsedTime transferTimer = new ElapsedTime();
+    public OuttakeState2 outtakeState2 = OuttakeState2.OUTTAKE2_IDLE;
+    public ElapsedTime outtakeTimer2 = new ElapsedTime();
+
 
     /** Build the paths for the auto (adds, for example, constant/linear headings while doing paths)
      * It is necessary to do this so that all the paths are built before the auto starts. **/
     public void buildPaths()
     {
+        /*
+        o.get1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(n.scorePosePT1), new Point(n.pickup2Pose)))
+                .setLinearHeadingInterpolation(n.scorePosePT1.getHeading(), n.pickup2Pose.getHeading())
+                .build();
 
+         */
+        o.park = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(o.startPose), new Point(o.parkPose)))
+                .setLinearHeadingInterpolation(o.startPose.getHeading(), o.parkPose.getHeading())
+                .build();
     }
 
     /** This switch is called continuously and runs the pathing, at certain points, it triggers the action state.
@@ -33,14 +180,26 @@ public class AutoObservationZone extends OpMode
      * The followPath() function sets the follower to run the specific path, but does NOT wait for it to finish before moving on. */
     public void autonomousPathUpdate()
     {
+        /*
+        switch (pathState)
+        {
 
-    }
+            //Park and End
+            case PARK:
+                if(pathTimer.getElapsedTime() >= 2000)
+                {
+                    follower.followPath(o.park);
 
-    /** These change the states of the paths and actions
-     * It will also reset the timers of the individual switches **/
-    public void setPathState(int pState) {
-        pathState = pState;
-        pathTimer.resetTimer();
+                    pathState = PathState.END;
+                }
+                break;
+            case END:
+                if(follower.getPose().getX() > (o.parkPose.getX() - 1) && follower.getPose().getY() > (o.parkPose.getY() - 1))
+                {
+                    //Set motors for teleop
+                }
+        }
+         */
     }
 
     /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
@@ -50,18 +209,66 @@ public class AutoObservationZone extends OpMode
         // These loop the movements of the robot
         follower.update();
         autonomousPathUpdate();
+        poseUpdater.update();
+        dashboardPoseTracker.update();
+
+        GrabSample();
+        Transfer();
+        ScoreSamplePT1();
+        ScoreSamplePT2();
+
+        //Set powers
+        setV4BPIDF(V4BTarget);
+        setSwivelPIDF(swivelTarget);
+        setVerticalSlidesPIDF(vertSlidesTarget);
+        r.rightLinear.setPosition(rightLinearTarget);
+        r.leftLinear.setPosition(leftLinearTarget);
+        r.claw.setPosition(clawTarget);
+        r.wrist.setPosition(wristTarget);
+        r.intake.setPower(intakePower);
 
         // Feedback to Driver Hub
+//        telemetry.addData("AA Intake Timer", intakeTimer.milliseconds());
+//        telemetry.addData("V4B Target", targetArmAngle);
+//        telemetry.addData("V4B Current Pos", currentArmAngle);
+//        telemetry.addData("V4B Power", V4BPower);
+//        telemetry.addData("path state", pathState);
+//        telemetry.addData("x", follower.getPose().getX());
+//        telemetry.addData("y", follower.getPose().getY());
+//        telemetry.addData("heading", Math.toDegrees(follower.getPose().getHeading()));
         telemetry.addData("path state", pathState);
-        telemetry.addData("x", follower.getPose().getX());
-        telemetry.addData("y", follower.getPose().getY());
-        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("intake state", intakeState);
+        telemetry.addData("transfer state", transferState);
+        telemetry.addData("outtake1 state", outtakeState);
+        telemetry.addData("outtake2 state", outtakeState2);
+
+        telemetry.addData("V4B Target", V4BTarget);
+        telemetry.addData("V4B Pos", r.rightV4BEnc.getCurrentPosition());
+        telemetry.addData("V4B Power", V4BPower);
+
+        //TelemetryPacket packet = new TelemetryPacket();
+        //packet.fieldOverlay().setStroke("#3F51B5");
+        //Drawing.drawRobot(packet.fieldOverlay(), follower.getPose());
+
         telemetry.update();
+
+        Drawing.drawPoseHistory(dashboardPoseTracker, "#4CAF50");
+        Drawing.drawRobot(poseUpdater.getPose(), "#4CAF50");
+        Drawing.sendPacket();
     }
 
     /** This method is called once at the init of the OpMode. **/
     @Override
     public void init() {
+        r = new ShoddyRobotClassAuto(this);
+        o = new AutoPositionsObservation();
+        po = new ShoddyPositions();
+        poseUpdater = new PoseUpdater(hardwareMap);
+        dashboardPoseTracker = new DashboardPoseTracker(poseUpdater);
+
+        Drawing.drawRobot(poseUpdater.getPose(), "#4CAF50");
+        Drawing.sendPacket();
+
         pathTimer = new Timer();
         opmodeTimer = new Timer();
 
@@ -72,23 +279,221 @@ public class AutoObservationZone extends OpMode
 
         buildPaths();
 
+        follower.setMaxPower(o.pedroSpeed);
+
         //other subsystems init
+
+        r.wheelSetUp();
+        r.servoSetUp();
+        r.motorSetUp();
+        r.analogSetUp();
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        controller = new PIDController(p, i, d);
+        controller2 = new PIDController(p2, i2, d2);
+        controller3 = new PIDController(p3, i3, d3);
+
+        r.topVertical.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        V4BTarget = po.V4B_TRANSFER_POS;
+        vertSlidesTarget = po.VERTICAL_REST;
+        swivelTarget = po.SWIVEL_DOWN;
+
+        leftLinearTarget = po.LEFT_SLIDE_IN;
+        rightLinearTarget = po.RIGHT_SLIDE_IN;
+        wristTarget = po.WRIST_PAR;
+
+        double botVerticalPower = 0;
+        double topVerticalPower = 0;
+        double intakePower = 0;
+
+        r.claw.setPosition(0.51);
     }
 
     /** This method is called continuously after Init while waiting for "play". **/
     @Override
-    public void init_loop() {}
+    public void init_loop()
+    {
+        clawTarget = 0.51;
+    }
 
     /** This method is called once at the start of the OpMode.
      * It runs all the setup actions, including building paths and starting the path system **/
     @Override
     public void start() {
         opmodeTimer.resetTimer();
-        setPathState(0);
+        //pathState = PathState.PARK;
+        follower.followPath(o.park);
+
+//        if (voltageSensor.getVoltage() > 13){
+//
+//        }
     }
 
     /** We do not use this because everything should automatically disable **/
     @Override
     public void stop() {
     }
+
+    public Pose poseOffset(Pose p)
+    {
+        return new Pose(p.getX()+xOffset,p.getY()+yOffset,p.getHeading()+hOffset);
+    }
+
+    public void GrabSample() {
+        switch (intakeState) {
+            case INTAKE_START:
+                leftLinearTarget = po.LEFT_SLIDE_OUT;
+                rightLinearTarget = po.RIGHT_SLIDE_OUT;
+                intakeTimer.reset();
+                intakeState = IntakeState.INTAKE_EXTEND;
+                break;
+            case INTAKE_EXTEND:
+                if (intakeTimer.milliseconds() >= extendTime) {
+                    intakePower = po.INTAKE_POWER_IN;
+                    V4BTarget = po.V4B_INTAKE_POS;
+                    intakeTimer.reset();
+                    intakeState = IntakeState.INTAKE_IDLE2;
+                }
+                break;
+            case INTAKE_GRAB:
+                intakePower = 0;
+                V4BTarget = po.V4B_REST_POS;
+                leftLinearTarget = po.LEFT_SLIDE_IN;
+                rightLinearTarget = po.RIGHT_SLIDE_IN;
+                intakeTimer.reset();
+                intakeState = IntakeState.INTAKE_RETRACT;
+                break;
+            case INTAKE_RETRACT:
+                if (intakeTimer.milliseconds() >= extendTime) {
+                    intakeState = IntakeState.INTAKE_IDLE;
+                }
+                break;
+        }
+    }
+
+    public void Transfer(){
+        switch (transferState) {
+            case TRANSFER_START:
+                intakePower=po.INTAKE_SLOW;
+
+                leftLinearTarget = po.LEFT_SLIDE_IN;
+                rightLinearTarget = po.RIGHT_SLIDE_IN;
+                V4BTarget = po.V4B_TRANSFER_POS;
+                transferState = TransferState.TRANSFER_INTAKE;
+                break;
+            case TRANSFER_INTAKE:
+                if (Math.abs(r.rightV4BEnc.getCurrentPosition() - po.V4B_TRANSFER_POS) <= 10) {
+                    clawTarget = po.CLAW_OPEN;
+                    vertSlidesTarget = po.VERTICAL_DOWN;
+                    transferState = TransferState.TRANSFER_CLAW;
+                }
+                break;
+            case TRANSFER_CLAW:
+                if (Math.abs(r.topVertical.getCurrentPosition() - po.VERTICAL_DOWN) <= 50) {
+                    clawTarget = po.CLAW_CLOSED;
+                    vertSlidesTarget = po.VERTICAL_REST;
+                    intakePower=0;
+                    transferState = TransferState.TRANSFER_OUTTAKE;
+                }
+                break;
+            case TRANSFER_OUTTAKE:
+                if (Math.abs(r.topVertical.getCurrentPosition() - po.VERTICAL_REST) <= 50) {
+                    transferState = TransferState.TRANSFER_IDLE;
+                }
+                break;
+        }
+    }
+
+    //ScoreSample
+    public void ScoreSamplePT1() {
+        switch (outtakeState) {
+            case OUTTAKE_START:
+                vertSlidesTarget = po.VERTICAL_UP;
+                swivelTarget = po.SWIVEL_UP;
+                wristTarget = po.WRIST_PERP;
+                outtakeTimer.reset();
+                outtakeState = OuttakeState.OUTTAKE_EXTEND;
+                break;
+            case OUTTAKE_EXTEND:
+                outtakeState = OuttakeState.OUTTAKE_SWIVEL;
+
+                break;
+            case OUTTAKE_SWIVEL:
+                if (outtakeTimer.milliseconds() >= extendTime+1300) {
+                    outtakeState = OuttakeState.OUTTAKE_IDLE;
+                }
+                break;
+        }
+    }
+
+    public void ScoreSamplePT2() {
+        switch (outtakeState2) {
+            case OUTTAKE2_START:
+                clawTarget = po.CLAW_CLOSED;
+                wristTarget = po.WRIST_PAR;
+                swivelTarget = po.SWIVEL_DOWN;
+                vertSlidesTarget = po.VERTICAL_REST;
+                outtakeState2 = OuttakeState2.OUTTAKE2_RETRACT;
+                break;
+            case OUTTAKE2_RETRACT:
+                if (Math.abs(r.topVertical.getCurrentPosition() - po.VERTICAL_REST) <= 50) {
+                    outtakeState2 = OuttakeState2.OUTTAKE2_IDLE;
+                }
+                break;
+        }
+    }
+
+
+    public void setV4BPIDF(int target)
+    {
+        controller.setPID(p, i, d);
+        armPos = r.rightV4BEnc.getCurrentPosition();
+        pid = controller.calculate(armPos, target);
+        targetArmAngle = target;
+        ff = (Math.sin(Math.toRadians(targetArmAngle))) * f;
+        currentArmAngle = Math.toRadians((armPos) / ticks_in_degree);
+
+        V4BPower = pid + ff;
+
+        r.leftArm.setPower(V4BPower);
+        r.rightArm.setPower(V4BPower);
+
+        //Telemetry
+    }
+
+    public void setVerticalSlidesPIDF(int target2)
+    {
+        controller2.setPID(p2, i2, d2);
+        armPos2 = r.topVertical.getCurrentPosition();
+        pid2 = controller2.calculate(armPos2, target2);
+        targetArmAngle2 = Math.toRadians((target2) / ticks_in_degree2);
+        ff2 = targetArmAngle2 * f2;
+        currentArmAngle2 = Math.toRadians((armPos2) / ticks_in_degree2);
+
+        verticalSlidesPower = pid2 + ff2;
+
+        r.topVertical.setPower(verticalSlidesPower);
+        r.bottomVertical.setPower(verticalSlidesPower);
+    }
+
+    public void setSwivelPIDF(int target3)
+    {
+        controller3.setPID(p3, i3, d3);
+        armPos3 = r.rightSwivelEnc.getCurrentPosition();
+        pid3 = controller3.calculate(armPos3, target3);
+        targetArmAngle3 = target3;
+        ff3 = (Math.cos(Math.toRadians(targetArmAngle3))) * f3;
+        currentArmAngle3 = Math.toRadians((armPos3) / ticks_in_degree3);
+
+        swivelPower = pid3 + ff3;
+
+        r.leftSwivel.setPower(swivelPower);
+        r.rightSwivel.setPower(swivelPower);
+    }
+
+    public double getVoltage() {
+        return voltageSensor.getVoltage();
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompOpModes/Autonomous/Positions/AutoPositionsObservation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompOpModes/Autonomous/Positions/AutoPositionsObservation.java
@@ -1,8 +1,24 @@
 package org.firstinspires.ftc.teamcode.CompOpModes.Autonomous.Positions;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 
 public class AutoPositionsObservation
 {
-    public final Pose startPose = new Pose(8.5, 87.5, Math.toRadians(0));
+    public double pedroSpeed = 1;
+
+    /*
+    |                                                                  ^
+    |              < +y                                          0 deg |
+    |                                <-- 90 (really 80) deg
+    |          ^                                               180 deg |
+    net       +x                                                       v
+    ___________________
+     */
+
+    public final Pose startPose = new Pose(8.5, 56.5, Math.toRadians(0));
+
+    public Pose parkPose = new Pose(11, 21, Math.toRadians(0));
+
+    public PathChain get1, get2, get3, park;
 }
